5eaccc78efae8fe0894e872514732562a969290a
jeoninho623 <0623inho@naver.com>
2023-10-20 15:04:52 +0900

    필수 항목 검증_리팩토링

--- src/test/java/tests/JoinServiceTest.java
+++ src/test/java/tests/JoinServiceTest.java
@@ -1,32 +1,29 @@
 package tests;
 
 import commons.BadRequestException;
-import commons.Validator;
-import models.member.JoinService;
-import models.member.JoinValidator;
-import models.member.Member;
-import models.member.MemberDao;
+import models.member.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@DisplayName("회원가입 기능 단위 테스트")
+@DisplayName("회원가입 기능 단위테스트")
 public class JoinServiceTest {
 
     private JoinService joinService;
 
     @BeforeEach
     void init() {
-        joinService = new JoinService(new JoinValidator(), new MemberDao());
+        joinService = ServiceManager.getInstance().joinService();
     }
 
     private Member getMember() {
+        String userPw = "12345678";
         return Member.builder()
                 .userId("user" + System.currentTimeMillis())
-                .userPw("12345678")
-                .confirmUserPw("12345678")
+                .userPw(userPw)
+                .confirmUserPw(userPw)
                 .userNm("사용자")
                 .email("user@test.org")
                 .agree(true)
@@ -42,16 +39,68 @@ public class JoinServiceTest {
     }
 
     @Test
-    @DisplayName("필수 항목 검증(아이디, 비밀번호, 비밀번호 확인, 회원명, 이메일, 회원가입약관 동의), 검증 실패 시 예외 발생 BadRequestException")
+    @DisplayName("필수 항목 검증(아이디, 비밀번호, 비밀번호 확인, 회원명, 이메일, 회원가입약관 동의), 검증 실패시 BadRequestException 발생")
     void requiredFieldCheck() {
-        // 아이디(userId) 검증 null 또는 빈값("")
-        assertThrows(BadRequestException.class, () -> {
-            Member member = getMember();
-            member.setUserId(null);
-            joinService.join(member);
+        assertAll(
+                () -> {
+                    // 아이디 검증(userId)
+                    Member member = getMember();
+                    member.setUserId(null);
+                    requiredFieldEachCheck(member, "아이디");
 
-            member.setUserId("  ");
+                    member.setUserId("   ");
+                    requiredFieldEachCheck(member,"아이디");
+
+                },() -> {
+                    // 비밀번호 검증(userPw)
+                    Member member = getMember();
+                    member.setUserPw(null);
+                    requiredFieldEachCheck(member, "비밀번호");
+
+                    member.setUserPw("   ");
+                    requiredFieldEachCheck(member,"비밀번호");
+
+                },() -> {
+                    // 비밀번호 확인(confirmUserPw)
+                    Member member = getMember();
+                    member.setConfirmUserPw(null);
+                    requiredFieldEachCheck(member, "비밀번호를 확인");
+
+                    member.setConfirmUserPw("   ");
+                    requiredFieldEachCheck(member,"비밀번호를 확인");
+
+                },() -> {
+                    // 회원명 검증(userNm)
+                    Member member = getMember();
+                    member.setUserNm(null);
+                    requiredFieldEachCheck(member, "이름");
+
+                    member.setUserNm("   ");
+                    requiredFieldEachCheck(member,"이름");
+
+                },() -> {
+                    // 이메일 검증(Email)
+                    Member member = getMember();
+                    member.setEmail(null);
+                    requiredFieldEachCheck(member, "이메일");
+
+                    member.setEmail("   ");
+                    requiredFieldEachCheck(member,"이메일");
+
+                },() -> {
+                    // 약관 동의 검증(agree)
+                    Member member = getMember();
+                    member.setAgree(false);
+                    requiredFieldEachCheck(member, "약관");
+                }
+        );
+
+    }
+
+    private void requiredFieldEachCheck(Member member, String word) {
+        BadRequestException thrown = assertThrows(BadRequestException.class, () -> {
             joinService.join(member);
         });
+        assertTrue(thrown.getMessage().contains(word));
     }
 }
